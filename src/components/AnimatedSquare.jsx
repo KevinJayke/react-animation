35a8fd8985dc3373f2e71e82fab995006d0ca3ed
KevinJayke <kevin.jayat@gmail.com>
2023-04-14 17:06:58 +0200

    refactor: square and got rid of use state

--- src/components/AnimatedSquare.jsx
+++ src/components/AnimatedSquare.jsx
@@ -1,5 +1,4 @@
-import { useSpring, animated } from "@react-spring/web";
-import { useState } from "react";
+import { useSpring, animated, useSpringRef } from "@react-spring/web";
 import styled from "styled-components";
 
 const Square = styled(animated.div)`
@@ -11,42 +10,25 @@ const Square = styled(animated.div)`
 `;
 
 const AnimatedSquare = () => {
-  const [springs, api] = useSpring(() => ({
+  const api = useSpringRef();
+  const springs = useSpring({
+    ref: api,
     from: { x: 0 },
-  }));
-  const [isReverse, setIsReverse] = useState(false);
+  });
 
   const handleClick = () => {
-    if (!isReverse) {
-      api.start({
-        from: {
-          x: 0,
-        },
-        to: {
-          x: 100,
-        },
-      });
-      setIsReverse(true);
-    }
-  };
+    const isEndingPosition = springs.x.get() === 200;
 
-  const handleClickReverse = () => {
-    if (isReverse) {
-      api.start({
-        from: {
-          x: 100,
-        },
-        to: {
-          x: 0,
-        },
-      });
-      setIsReverse(false);
-    }
+    api.start({
+      to: {
+        x: isEndingPosition ? 0 : 200,
+      },
+    });
   };
 
   return (
     <Square
-      onClick={isReverse ? handleClickReverse : handleClick}
+      onClick={handleClick}
       style={{
         ...springs,
       }}
