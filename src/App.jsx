7d645d44c6e088f5cb3c1d27dd92c0f0e3167ba4
KevinJayke <kevin.jayat@gmail.com>
2023-04-14 16:22:38 +0200

    feat:animating a basic square

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import AnimatedDiv from "./components/AnimatedDiv";
+
 function App() {
-  return <div className="App">Simple</div>;
+  return (
+    <div className="App">
+      <AnimatedDiv text={"salut"} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AnimatedDiv.jsx
@@ -0,0 +1,56 @@
+import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Square = styled(animated.div)`
+  width: 80px;
+  height: 80px;
+  background-color: #ff6d6d;
+  border-radius: 8px;
+`;
+
+const MyComponent = () => {
+  const [springs, api] = useSpring(() => ({
+    from: { x: 0 },
+  }));
+  const [isReverse, setIsReverse] = useState(false);
+
+  const handleClick = () => {
+    if (!isReverse) {
+      api.start({
+        from: {
+          x: 0,
+        },
+        to: {
+          x: 100,
+        },
+      });
+      setIsReverse(true);
+    }
+  };
+
+  const handleClickReverse = () => {
+    if (isReverse) {
+      api.start({
+        from: {
+          x: 100,
+        },
+        to: {
+          x: 0,
+        },
+      });
+      setIsReverse(false);
+    }
+  };
+
+  return (
+    <Square
+      onClick={isReverse ? handleClickReverse : handleClick}
+      style={{
+        ...springs,
+      }}
+    />
+  );
+};
+
+export default MyComponent;
